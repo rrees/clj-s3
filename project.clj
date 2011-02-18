8f707016c531b4a550c4ba74826429af36c3c673
Robert Rees <spam@shudderfix.com>
2011-02-18 22:47:57 +0000

    Added the AWS SDK as a dependency

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
 (defproject clj-s3 "0.0.1-SNAPSHOT"
   :description "TODO: add summary of your project"
-  :dependencies [[clojure "1.2.0"]])
+  :dependencies [[clojure "1.2.0"] [com.amazonaws/aws-java-sdk "1.1.6"]])
